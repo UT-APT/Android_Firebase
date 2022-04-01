9e75bec8dd705f984f566e6372dbf02c411e0683
graceleee <leegracew@gmail.com>
2022-04-01 10:45:13 -0500

    changed play service auth version
    otherwise Android API 31 FLAG_IMMUTABLE error

--- app/src/main/java/com/example/babyfirebase/MainActivity.kt
+++ app/src/main/java/com/example/babyfirebase/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.babyfirebase
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 import android.widget.Toast
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
@@ -35,25 +36,38 @@ class MainActivity : AppCompatActivity() {
         // Initialize Firebase Auth
         auth = Firebase.auth
         // [END initialize_auth]
+
+        val signInButton: Button = findViewById(R.id.signInButton)
+        val signOutButton: Button = findViewById(R.id.signOutButton)
+        signInButton.setOnClickListener { startSignIn() }
+
     }
 
-    private fun createSignInIntent() {
-        // [START auth_fui_create_intent]
-        // Choose authentication providers
-        val providers = arrayListOf(
-            AuthUI.IdpConfig.EmailBuilder().build(),
-            AuthUI.IdpConfig.PhoneBuilder().build(),
-            AuthUI.IdpConfig.GoogleBuilder().build(),
-            AuthUI.IdpConfig.FacebookBuilder().build(),
-            AuthUI.IdpConfig.TwitterBuilder().build())
+//    private fun createSignInIntent() {
+//        // [START auth_fui_create_intent]
+//        // Choose authentication providers
+//        val providers = arrayListOf(
+//            AuthUI.IdpConfig.EmailBuilder().build(),
+//            AuthUI.IdpConfig.PhoneBuilder().build(),
+//            AuthUI.IdpConfig.GoogleBuilder().build(),
+//            AuthUI.IdpConfig.FacebookBuilder().build(),
+//            AuthUI.IdpConfig.TwitterBuilder().build())
+//
+//        // Create and launch sign-in intent
+//        val signInIntent = AuthUI.getInstance()
+//            .createSignInIntentBuilder()
+//            .setAvailableProviders(providers)
+//            .build()
+//        signInLauncher.launch(signInIntent)
+//        // [END auth_fui_create_intent]
+//    }
 
-        // Create and launch sign-in intent
+    private fun startSignIn() {
         val signInIntent = AuthUI.getInstance()
             .createSignInIntentBuilder()
-            .setAvailableProviders(providers)
+            .setAvailableProviders(listOf(AuthUI.IdpConfig.EmailBuilder().build()))
             .build()
         signInLauncher.launch(signInIntent)
-        // [END auth_fui_create_intent]
     }
 
     // [START auth_fui_result]
@@ -72,27 +86,27 @@ class MainActivity : AppCompatActivity() {
     }
     // [END auth_fui_result]
 
-    private fun signOut() {
-        // [START auth_fui_signout]
-        AuthUI.getInstance()
-            .signOut(this)
-            .addOnCompleteListener {
-                // ...
-            }
-        // [END auth_fui_signout]
-    }
+//    private fun signOut() {
+//        // [START auth_fui_signout]
+//        AuthUI.getInstance()
+//            .signOut(this)
+//            .addOnCompleteListener {
+//                // ...
+//            }
+//        // [END auth_fui_signout]
+//    }
 
     // [START on_start_check_user]
-    public override fun onStart() {
-        super.onStart()
-        // Check if user is signed in (non-null) and update UI accordingly.
-        val currentUser = auth.currentUser
-        if(currentUser != null){
-            reload()
-        } else {
-            // No user is signed in
-        }
-    }
+//    public override fun onStart() {
+//        super.onStart()
+//        // Check if user is signed in (non-null) and update UI accordingly.
+//        val currentUser = auth.currentUser
+//        if(currentUser != null){
+//            reload()
+//        } else {
+//            // No user is signed in
+//        }
+//    }
     // [END on_start_check_user]
 
     private fun createAccount(email: String, password: String) {
@@ -135,35 +149,35 @@ class MainActivity : AppCompatActivity() {
         // [END sign_in_with_email]
     }
 
-    private fun sendEmailVerification() {
-        // [START send_email_verification]
-        val user = auth.currentUser!!
-        user.sendEmailVerification()
-            .addOnCompleteListener(this) { task ->
-                // Email Verification sent
-            }
-        // [END send_email_verification]
-    }
+//    private fun sendEmailVerification() {
+//        // [START send_email_verification]
+//        val user = auth.currentUser!!
+//        user.sendEmailVerification()
+//            .addOnCompleteListener(this) { task ->
+//                // Email Verification sent
+//            }
+//        // [END send_email_verification]
+//    }
 
-    private fun getUserProfile() {
-        // [START get_user_profile]
-        val user = Firebase.auth.currentUser
-        user?.let {
-            // Name, email address, and profile photo Url
-            val name = user.displayName
-            val email = user.email
-            val photoUrl = user.photoUrl
-
-            // Check if user's email is verified
-            val emailVerified = user.isEmailVerified
-
-            // The user's ID, unique to the Firebase project. Do NOT use this value to
-            // authenticate with your backend server, if you have one. Use
-            // FirebaseUser.getToken() instead.
-            val uid = user.uid
-        }
-        // [END get_user_profile]
-    }
+//    private fun getUserProfile() {
+//        // [START get_user_profile]
+//        val user = Firebase.auth.currentUser
+//        user?.let {
+//            // Name, email address, and profile photo Url
+//            val name = user.displayName
+//            val email = user.email
+//            val photoUrl = user.photoUrl
+//
+//            // Check if user's email is verified
+//            val emailVerified = user.isEmailVerified
+//
+//            // The user's ID, unique to the Firebase project. Do NOT use this value to
+//            // authenticate with your backend server, if you have one. Use
+//            // FirebaseUser.getToken() instead.
+//            val uid = user.uid
+//        }
+//        // [END get_user_profile]
+//    }
 
     private fun updateUI(user: FirebaseUser?) {
 
